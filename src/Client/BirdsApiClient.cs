d220605c208601660f85ed638536fca75565f277
Dmitry <d.seleznev6@g.nsu.ru>
2019-12-17 23:43:09 +0700

    add httpClient

--- src/Client/BirdsApiClient.cs
+++ src/Client/BirdsApiClient.cs
@@ -1,7 +1,14 @@
+using System.Net.Http;
+
 namespace Client
 {
-    public class BirdsApiClient
+    public sealed class BirdsApiClient
     {
-        
+        private readonly HttpClient httpClient;
+
+        public BirdsApiClient(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
     }
 }
