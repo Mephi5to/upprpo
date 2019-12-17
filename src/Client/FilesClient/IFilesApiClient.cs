d7860d03574fa567db81787eb852449e341c683e
Dmitry <d.seleznev6@g.nsu.ru>
2019-12-18 00:25:44 +0700

    add files client interface

---
+++ src/Client/FilesClient/IFilesApiClient.cs
@@ -0,0 +1,11 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Birds.ClientModels.Birds;
+using ClientModels = Birds.ClientModels.Files;
+
+namespace Client.FilesClient
+{
+    public interface IFilesApiClient
+    {
+    }
+}
