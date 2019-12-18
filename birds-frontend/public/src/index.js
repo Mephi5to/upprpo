3ec12f335910f11d2a7107537bc4c847c7373861
i.stoliarov <stoliarov.ilya@yandex.ru>
2019-12-18 13:07:44 +0700

    add Body component using Item

--- birds-frontend/public/src/index.js
+++ birds-frontend/public/src/index.js
@@ -13,13 +13,32 @@ class App extends React.Component {
 
     render() {
         return <div>
+            <Body
+                birdsListProps={this.state.birdsList}
+                setSelectedBirdFunc={this.setSelectedBird}>
+            </Body>
         </div>
     }
+
+    setSelectedBird = (bird) => {
+        this.setState({
+            selectedBird: bird
+        })
+    }
 }
+
+const Body = (props) => {
+    return <div>
+        {props.birdsListProps.map((bird) =>
+            <Item
+                bird={bird}
+                setSelectedBirdFunc={props.setSelectedBirdFunc}>
+            </Item>)
+        }
+    </div>
+};
 
 
 
 
 ReactDOM.render(<App/>, document.getElementById('root'));
-
-
