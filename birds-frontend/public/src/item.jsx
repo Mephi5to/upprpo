b8366bd46f9c723ca05851973b721ee22280d4a5
i.stoliarov <stoliarov.ilya@yandex.ru>
2019-12-18 12:43:39 +0700

    add bird name to Item

--- birds-frontend/public/src/item.jsx
+++ birds-frontend/public/src/item.jsx
@@ -8,8 +8,15 @@ const Item = (props) => {
         <div className="horizont_float">
         </div>
         <div className="item_body">
+            <ItemHeader name={props.bird.name}></ItemHeader>
         </div>
     </div>
+};
+
+const ItemHeader = (props) => {
+    return <h2 id={props.id} className="bird_name">
+        <span className="bird_name_text">{props.name}</span>
+    </h2>
 };
 
 export default Item;
