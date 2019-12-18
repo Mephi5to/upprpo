b670f674c1a698ada5e85a5fff9e4ce9499fe751
i.stoliarov <stoliarov.ilya@yandex.ru>
2019-12-18 12:10:50 +0700

    add main react component with initial styles

---
+++ birds-frontend/public/src/index.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+class App extends React.Component {
+
+    constructor() {
+        super()
+    }
+
+    render() {
+        return <div>
+        </div>
+    }
+}
+
+
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+
