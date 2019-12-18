67108a698e056fbe56c7924b37f1984f33c4f555
Dmitry <d.seleznev6@g.nsu.ru>
2019-12-18 21:39:19 +0700

    birds upload method

--- src/FilesTool/Loader.cs
+++ src/FilesTool/Loader.cs
@@ -1,7 +1,38 @@
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Birds.ClientModels.Birds;
+using Newtonsoft.Json;
+
 namespace FilesTool
 {
-    public sealed class Loader
+    public static class Loader
     {
+        private const string Url = "https://localhost:5001";
+        private const string imagesDirectoryPath = "D:\\NSU\\УППРПО\\Images";
+        private const string soundsDirectoryPath = "D:\\NSU\\УППРПО\\Sounds";
+        private const string birdsData = "bird_data.json";
         
+        public static async Task Upload()
+        {
+            
+        }
+        
+        private static async Task UploadBirdsToServer()
+        {
+            var batchBuildInfo = JsonConvert.DeserializeObject<BatchBirdsBuildInfo>(File.ReadAllText(birdsData));
+            var json = JsonConvert.SerializeObject(batchBuildInfo);
+            var stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response;
+            
+            using (var client = new HttpClient())
+            {
+                response = await client.PostAsync(Url + "/api/v1/birds", stringContent).ConfigureAwait(false);
+            }
+            
+            Console.WriteLine(response.StatusCode);
+        }
     }
 }
