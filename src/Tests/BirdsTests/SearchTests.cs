2699016ce6452f0032d0473b16081cb7df32a84f
Dmitry <d.seleznev6@g.nsu.ru>
2019-12-20 01:10:10 +0700

    add search birds test

---
+++ src/Tests/BirdsTests/SearchTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+ using System.Threading.Tasks;
+ using Birds.ClientModels.Birds;
+ using NUnit.Framework;
+
+ namespace Tests.BirdsTests
+{
+    [TestFixture]
+    [SingleThreaded]
+    [Parallelizable(ParallelScope.None)]
+    public class SearchTests : TestBase
+    {
+        [Test]
+        public async Task SearchWithLimitTestAsync()
+        {
+            var offset = 0;
+            var limit = 2;
+            var searchQuery = new BirdsSearchQuery {Limit = limit, Offset = offset};
+            var result = await this.BirdsApiClient.SearchBirdsAsync(searchQuery, CancellationToken.None).ConfigureAwait(false);
+
+            Assert.NotNull(result);
+            Assert.AreEqual(limit, result.Birds.Count);
+        }
+    }
+}
